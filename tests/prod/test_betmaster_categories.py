d0a46949932fa75dafcbae272fba770cc2c7f8e7
Juri Svetlik <jurisvetlik@Juris-MacBook-Air.local>
2024-05-29 01:27:33 +0300

    minor fix

--- tests/prod/test_betmaster_categories.py
+++ tests/prod/test_betmaster_categories.py
@@ -10,9 +10,9 @@ class TestBetmasterCategories(BaseTest):
     @pytest.mark.parametrize("category", ["rtp98", "rtp98wrong"])
     async def test_betmaster_category_rpt98(self, page, category):
         self.description("Open category RTP98+ and get game images in the category")
-        slot_page = SlotsPage(page)
-        await slot_page.categories.rtp98plus.click()
-        frontend_games_count = await slot_page.game_img.elements_count(wait_before=500)
+        slots_page = SlotsPage(page)
+        await slots_page.categories.rtp98plus.click()
+        frontend_games_count = await slots_page.game_img.elements_count(wait_before=500)
 
         self.description("MOCK: Send request to backend to understand count of games in the category RTP98+")
         _, resp_body = RestApi().get_games_in_category(category)
